b144689cc074e731080dd033f8ab26c59ae39925
Dan Rosenbaum <drosenbaum5@gmail.com>
2021-04-05 11:28:45 -0400

    Set up initial files and successfully tested API call

---
+++ script.js
@@ -0,0 +1,7 @@
+fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+    .then(function(response){
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data);
+    })
