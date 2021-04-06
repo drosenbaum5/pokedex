d02946411b000f1912fac9a64031a7124c0a4f5c
Dan Rosenbaum <drosenbaum5@gmail.com>
2021-04-06 10:27:20 -0400

    Added function for calling single pokemon. Added additional function with for loop for getting all pokemon and rendering an image to the screen

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-    .then(function(response){
-        return response.json()
+const mainUrl = "https://pokeapi.co/api/v2/pokemon/";
+
+function renderAllPokemon() {
+  for (let i = 1; i < 200; i++) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (pokemon) {
+        // console.log(data);
+        // console.log(pokemon);
+        // console.log(pokemon.sprites.other.dream_world.front_default);
+        console.log(pokemon);
+        console.log(pokemon.sprites.other.dream_world.front_default);
+        const createPokemonImg = document.createElement("img");
+        createPokemonImg.src = pokemon.sprites.other.dream_world.front_default;
+        const pokemonContainer = document.getElementById("all-pokemon");
+        pokemonContainer.append(createPokemonImg);
+      });
+  }
+}
+
+function renderOnePokemon() {
+  fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`)
+    .then(function (response) {
+      return response.json();
     })
-    .then(function (data) {
-        console.log(data);
-    })
+    .then(function (pokemon) {
+      // console.log(data);
+      console.log(pokemon);
+      console.log(pokemon.sprites.other.dream_world.front_default);
+      const createPokemonImg = document.createElement("img");
+      createPokemonImg.src = pokemon.sprites.other.dream_world.front_default;
+      const pokemonContainer = document.getElementById("all-pokemon");
+      pokemonContainer.append(createPokemonImg);
+    });
+}
+
+renderAllPokemon();
+// renderOnePokemon();
