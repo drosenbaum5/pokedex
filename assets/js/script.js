2989baa7ca7f13b751b781c0b538e9bd47dcafff
Dan Rosenbaum <drosenbaum5@gmail.com>
2021-04-07 18:48:37 -0400

    Added dynamic span tags wrappfunctions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,29 +7,33 @@ function renderAllPokemon() {
         return response.json();
       })
       .then(function (pokemon) {
-        
         console.log(pokemon);
-        console.log(pokemon.sprites.other.dream_world.front_default);
+        console.log(pokemon.types[0].type.name);
 
-        
         const pokemonCard = document.createElement("div");
         pokemonCard.classList.add("card");
         const createPokemonImg = document.createElement("img");
         createPokemonImg.src = pokemon.sprites.other.dream_world.front_default;
-        const pokemonNameTag = document.createElement("h4");
+        const pokemonNameTag = document.createElement("h3");
         const pokemonName = pokemon.name.toUpperCase();
-        pokemonNameTag.textContent = pokemonName;
+        pokemonNameTag.textContent = pokemon.id + ". " + pokemonName;
         pokemonCard.appendChild(createPokemonImg);
         pokemonCard.appendChild(pokemonNameTag);
 
+        const typeContainer = document.createElement("div");
+        typeContainer.classList.add("type-container");
+        pokemonCard.appendChild(typeContainer);
+        for (let i = 0; i < pokemon.types.length; i++) {
+          const pokemonType = document.createElement("span");
+          pokemonType.textContent = pokemon.types[i].type.name;
+          typeContainer.appendChild(pokemonType);
+        }
+
         const pokemonContainer = document.getElementById("all-pokemon");
         pokemonContainer.append(pokemonCard);
-
       });
   }
 }
-
-
 
 // const pokemonCard = document.createElement("div");
 // pokemonCard.classList.add("card");
@@ -38,10 +42,6 @@ function renderAllPokemon() {
 // pokemonCard.appendChild(createPokemonImg);
 // const pokemonContainer = document.getElementById("all-pokemon");
 // pokemonContainer.append(pokemoncard);
-        
-
-
-
 
 function renderOnePokemon() {
   fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`)
@@ -50,12 +50,27 @@ function renderOnePokemon() {
     })
     .then(function (pokemon) {
       // console.log(data);
-      console.log(pokemon);
-      console.log(pokemon.sprites.other.dream_world.front_default);
+      const pokemonCard = document.createElement("div");
+      pokemonCard.classList.add("card");
       const createPokemonImg = document.createElement("img");
       createPokemonImg.src = pokemon.sprites.other.dream_world.front_default;
+      const pokemonNameTag = document.createElement("h3");
+      const pokemonName = pokemon.name.toUpperCase();
+      pokemonNameTag.textContent = pokemon.id + ". " + pokemonName;
+      pokemonCard.appendChild(createPokemonImg);
+      pokemonCard.appendChild(pokemonNameTag);
+
+      const typeContainer = document.createElement("div");
+      typeContainer.classList.add("type-container");
+      pokemonCard.appendChild(typeContainer);
+      for (let i = 0; i < pokemon.types.length; i++) {
+        const pokemonType = document.createElement("span");
+        pokemonType.textContent = pokemon.types[i].type.name;
+        typeContainer.appendChild(pokemonType);
+      }
+
       const pokemonContainer = document.getElementById("all-pokemon");
-      pokemonContainer.append(createPokemonImg);
+      pokemonContainer.append(pokemonCard);
     });
 }
 
