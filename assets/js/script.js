2a4476ac9c223ed6c630b85f7082b134ae05a6e1
Dan Rosenbaum <drosenbaum5@gmail.com>
2021-04-08 08:47:04 -0400

    Created new function to consolidate pokemon card rendering. Removed reduntant code and insert mainURL variable into API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,106 +1,63 @@
 const mainUrl = "https://pokeapi.co/api/v2/pokemon/";
 
-// CSS classes to determine pokemon type styling
-const classTypes = [
-  "dark",
-  "dragon",
-  "rock",
-  "ice",
-  "ghost",
-  "psychic",
-  "fighting",
-  "water",
-  "fairy",
-  "electric",
-  "normal",
-  "bug",
-  "flying",
-  "fire",
-  "poison",
-  "grass",
-];
 
 // Render all available pokemon from API
 function renderAllPokemon() {
   for (let i = 1; i <= 200; i++) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+    fetch(`${mainUrl}${i}`)
       .then(function (response) {
         return response.json();
       })
-      .then(function (pokemon) {
+      .then(function (data) {
         // console.log(pokemon);
         // console.log(pokemon.types[0].type.name);
-
-        // Create div to hold card and add class list of card
-        const pokemonCard = document.createElement("div");
-        pokemonCard.classList.add("card");
-
-        const typeContainer = document.createElement("div");
-        typeContainer.classList.add("type-container");
-        pokemonCard.appendChild(typeContainer);
-
-        for (let i = 0; i < pokemon.types.length; i++) {
-          const pokemonType = document.createElement("span");
-          pokemonType.textContent = pokemon.types[i].type.name;
-          pokemonType.classList.add(pokemonType.textContent);
-          typeContainer.appendChild(pokemonType);
-        }
-        // Create image and set src attribute from API data
-        const createPokemonImg = document.createElement("img");
-        createPokemonImg.src = pokemon.sprites.other.dream_world.front_default;
-        // Create h3 and set text content from API data
-        const pokemonNameTag = document.createElement("h3");
-        const pokemonName = pokemon.name.toUpperCase();
-        pokemonNameTag.textContent = pokemon.id + ". " + pokemonName;
-        // Append everything to the card
-        pokemonCard.appendChild(createPokemonImg);
-        pokemonCard.appendChild(pokemonNameTag);
-
-        // Create div to hold span tags which indicate pokemon type
-        // Add class list of container and append to the card
-        
-
-        // Determine class to add based on pokemon type
-        
-
-        const pokemonContainer = document.getElementById("all-pokemon");
-        pokemonContainer.append(pokemonCard);
+        renderPokemon(data);
       });
   }
 }
 
+function renderPokemon(pokemon) {
+  // Create div to hold card and add class list of card
+  const pokemonCard = document.createElement("div");
+  pokemonCard.classList.add("card");
+
+  const typeContainer = document.createElement("div");
+  typeContainer.classList.add("type-container");
+  pokemonCard.appendChild(typeContainer);
+
+  for (let i = 0; i < pokemon.types.length; i++) {
+    const pokemonType = document.createElement("span");
+    pokemonType.textContent = pokemon.types[i].type.name;
+    pokemonType.classList.add(pokemonType.textContent);
+    typeContainer.appendChild(pokemonType);
+  }
+  // Create image and set src attribute from API data
+  const createPokemonImg = document.createElement("img");
+  createPokemonImg.src = pokemon.sprites.other.dream_world.front_default;
+  // Create h3 and set text content from API data
+  const pokemonNameTag = document.createElement("h3");
+  const pokemonName = pokemon.name.toUpperCase();
+  pokemonNameTag.textContent = pokemon.id + ". " + pokemonName;
+  // Append everything to the card
+  pokemonCard.appendChild(createPokemonImg);
+  pokemonCard.appendChild(pokemonNameTag);
+
+  // Create div to hold span tags which indicate pokemon type
+  // Add class list of container and append to the card
+
+  // Determine class to add based on pokemon type
+
+  const pokemonContainer = document.getElementById("all-pokemon");
+  pokemonContainer.append(pokemonCard);
+}
+
 function renderOnePokemon() {
-  fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`)
+  fetch(`${mainUrl}pikachu`)
     .then(function (response) {
       return response.json();
     })
-    .then(function (pokemon) {
-      const pokemonCard = document.createElement("div");
-      pokemonCard.classList.add("card");
-      const createPokemonImg = document.createElement("img");
-      createPokemonImg.src = pokemon.sprites.other.dream_world.front_default;
-      const pokemonNameTag = document.createElement("h3");
-      const pokemonName = pokemon.name.toUpperCase();
-      pokemonNameTag.textContent = pokemon.id + ". " + pokemonName;
-      pokemonCard.appendChild(createPokemonImg);
-      pokemonCard.appendChild(pokemonNameTag);
-
-      const typeContainer = document.createElement("div");
-      typeContainer.classList.add("type-container");
-      pokemonCard.appendChild(typeContainer);
-      for (let i = 0; i < pokemon.types.length; i++) {
-        const pokemonType = document.createElement("span");
-        pokemonType.textContent = pokemon.types[i].type.name;
-        console.log(pokemonType.value);
-        if (pokemonType.value == "grass") {
-          console.log("It's a grass pokemon!");
-          pokemonType.classList.add("grass");
-        }
-        typeContainer.appendChild(pokemonType);
-      }
-
-      const pokemonContainer = document.getElementById("all-pokemon");
-      pokemonContainer.append(pokemonCard);
+    .then(function (data) {
+      renderPokemon(data);
     });
 }
 
